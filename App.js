bbb8d4712031b37652aa77213a39c368b2f03768
MB111774 <maciej.budzinski.93@gmail.com>
2018-06-27 12:40:31 +0200

    initial commit with eslint and preetier configuration

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload, Shake or press menu button for dev menu',
+});
+function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome </Text>
+      <Text style={styles.instructions}>To get started, edit App asddsasd</Text>
+      <Text style={styles.instructions}>{instructions}</Text>
+    </View>
+  );
+}
+export default App;
