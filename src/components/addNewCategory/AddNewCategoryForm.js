c83b1a87de8a6fd379cd7f0d6d2428716953648f
MB111774 <maciej.budzinski.93@gmail.com>
2018-07-03 08:38:15 +0200

    category form fix

--- src/components/addNewCategory/AddNewCategoryForm.js
+++ src/components/addNewCategory/AddNewCategoryForm.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 import { Content, Text, Form, Item, Label, Input, Button, View } from 'native-base';
 import { ColorWheel } from 'react-native-color-wheel';
 
+const rgb = require('hsv-rgb');
+
 class AddNewCategoryForm extends Component {
   state = {
     categoryName: '',
-    categoryColor: '',
+    categoryColor: '#00ee00',
+  };
+
+  onChange = (hM, s, v) => {
+    let h = hM;
+    if (hM < 0) {
+      console.log('ok');
+      h += 360;
+    }
+    const rgbColor = rgb(h, s, v);
+    const rgbColorToState = `rgb(${rgbColor[0]},${rgbColor[1]},${rgbColor[2]})`;
+    this.setState({
+      categoryColor: rgbColorToState,
+    });
+  };
+
+  onSubmit = () => {
+    const { categoryName, categoryColor } = this.state;
+    console.log(categoryName, categoryColor);
   };
 
   render() {
@@ -14,7 +34,7 @@ class AddNewCategoryForm extends Component {
       <Content>
         <Form>
           <Item stackedLabel>
-            <Label>Nazwa</Label>
+            <Label style={{ color: `${categoryColor}` }}>Nazwa</Label>
             <Input
               value={categoryName}
               onChangeText={text => this.setState({ categoryName: text })}
@@ -23,10 +43,18 @@ class AddNewCategoryForm extends Component {
           <Item stackedLabel>
             <Label>Kolor kategorii</Label>
             <View style={{ alignItems: 'center', height: 250 }}>
-              <ColorWheel initialColor="#00ee00" style={{ height: 200, width: 200 }} />
+              <ColorWheel
+                style={{ height: 200, width: 200 }}
+                initialColor="#00ee00"
+                onColorChange={color => this.onChange(color.h, color.s, color.v)}
+              />
             </View>
           </Item>
-          <Button style={{ width: `100%`, alignItems: 'center', justifyContent: 'center' }} primary>
+          <Button
+            style={{ width: `100%`, alignItems: 'center', justifyContent: 'center' }}
+            primary
+            onPress={this.onSubmit}
+          >
             <Text>Dodaj</Text>
           </Button>
         </Form>
